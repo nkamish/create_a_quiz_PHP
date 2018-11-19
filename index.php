3b4f4af11eb1d6914a5465392aa0c466a5afc544
giordifungula <giordifungula@gmail.com>
2018-11-19 10:04:17 +0200

    created questions

---
+++ index.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Assassin's Creed Quiz</title>
+    <link rel="stylesheet" href="css/main.css">
+
+    <style>
+        body {
+            background-color: #46474c;
+        }
+        p {
+            color: rgb(217, 217, 223);
+        }
+        .radio {
+            color: rgb(217, 217, 223);
+        }
+        .hero {
+            background-image: url('img/hero.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+
+        }
+
+        .hero-body > .container {
+            position: absolute;
+            top: 50%;
+        }
+
+        .navbar-brand {
+            font-size: 34px;
+            font-weight: bold;
+        }
+
+        .hero-body > div > h1 {
+            color: white;
+        }
+        .hero-body > div > h2 {
+            color:dimgrey;
+        }
+
+        .footer {
+            color: white;
+            background-color: rgb(2, 70, 41);
+        }
+    </style>
+</head>
+<body>
+    <section class="hero is-fullheight">
+        <nav class="navbar is-success">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a href="#" class="navbar-item"><img src="img/logo.png" alt=""></a>
+
+                    <span class="navbar-burger burger" data-target="navMenu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+
+                </div>
+
+                <div class="navbar-menu" id="navMenu">
+                    <div class="navbar-end">
+                        <a href="#" class="navbar-item"></a>
+                        <a href="#" class="navbar-item">Galery</a>
+                        <a href="#" class="navbar-item">About</a>
+                        <a href="#" class="navbar-item">Contact</a>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+
+        <div class="hero-body">
+            <div class="container">
+              
+                    <h1 class="title">
+                        Natheer Kamish Animations
+                    </h1>
+                    <h2 class="subtitle">
+                        Capturing moments | Creating Memes
+                    </h2>
+           
+            </div>
+        </div>
+
+    </section>
+
+    
+
+    <p>1. Why did Altair lose his rank in the beginning of the first game?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        He lost a battle against a target
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        He wasn't good at sneaking anymore
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        He broke the tenets of the Creed
+    </label>
+    </div>
+
+    <p>2. Who did Ezio have a fist fight with as a young adult?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Vieri de' Pazzi
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Giovanni Tornabuoni
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Leonardo da Vinci
+    </label>
+    </div>
+
+    <p>3. Where was the primary setting of Assassin's Creed Brotherhood?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Rome
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Italy
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Greece
+    </label>
+    </div>
+
+    <p>4. Who was the secondary assassin that you play as in Assassin's Creed Revelations?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Conner
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Altair
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Edward
+    </label>
+    </div>
+
+    <p>5. What region was Achilles Davenport (Conner's mentor) born in?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Early American Colonies
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        The Caribbean
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        England
+    </label>
+    </div>
+
+    <p>6. Who was NOT a pirate who teamed up with Edward Kenway in Assassin's Creed 4?</p>        
+
+    
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Calico Jack
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Henry Every
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Bartholowmew Roberts
+    </label>
+    </div>
+
+    <p>7. Which piece of Eden is featured in Assassin's Creed Unity?</p>
+    
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Apple of Eden
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Crystal Skull of Eden
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Sword of Eden
+    </label>
+    </div>
+
+    <p>8. What was the name of the gang that Jacob Frye started in Assassin's Creed Syndicate?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        The Rooks
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        The Eagles
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        The Assassins
+    </label>
+
+    <p>9. What death shaped Bayek's motives in the early stages of Assassin's Creed Origin?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        His wife's death
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        His son's death
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        His friend's death
+    </label>    
+
+    <p>10. In what specific City State does the main protagonist originate from in Assassins Creed Odyssey?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Thebes
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Sparta
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Athens
+    </label>
+	
+<p>11. Who is the first Templar you play as in the Assassin's Creed series?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Haytham Kenway
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Edward Kenway
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Rodrigo Borgia
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Otso Berg
+    </label>
+  
+    <p>12. Who is the first person you personally kill in Black Flag?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Duncan Walpole
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        El Tiburon
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Rodrigo Borgia
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Shay Cormac
+    </label>
+  
+    <p>13. Who is the final person wounded in Assassin's Creed: Rogue?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Liam O'Brien
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Haytham Kenway
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Achilles Davenport
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Hope Jensen
+    </label>
+
+    <p>14. Who is the final target in Assassin's Creed: Unity?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Thomas Francois-Germain
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Arno Dorian
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Benjamin Hornigold
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Elise de la Serre
+    </label>
+
+    <p>15. What is the name of Edward Kenway's ship?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        The Jackbird
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        The Jackdaw
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        The Black Pearl
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Her Majesty
+    </label>
+ 
+    <p>16. How does Ezio's father die?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        He is assassinated
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Sickness
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        He is hanged
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        He doesn't
+    </label>
+ 
+    <p>17. In which game does Desmond die?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Assassin's Creed II
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Assassin's Creed: Revelations
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Assassin's Creed IV: Black Flag
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Assassin's Creed III
+    </label>
+ 
+    <p>18. In Assassin's Creed IV: Black Flag, what is the name of the island where the Assassins hide out?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Tulum
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Nasseau
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Havana
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Assassin Island
+    </label>
+
+    <p>19. James Kidd was in disguise in Assassin's Creed IV: Black Flag. What was he really?</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        A Templar
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        A woman
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        An old man
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Miley Cyrus!!! (HE CAME IN LIKE A WRECKING BALL!!!!!)
+    </label>
+
+    <p>Hehehe..... What is the color of Edward Kenway's hair?! -.-</p>
+
+    <div class="control">
+    <label class="radio">
+        <input type="radio" name="foobar">
+        Black
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Blonde
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        Red
+    </label>
+    <label class="radio">
+        <input type="radio" name="foobar" checked>
+        BALD!!!
+    </label>
+  
+  
+  
+</body>
+</html>
